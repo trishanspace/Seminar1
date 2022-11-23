0db8e3bed2c18eeebc440c8607c4c3d4f08c20b1
Nataliia <nataliyatrishechkina@mail.ru>
2022-11-23 10:58:32 +0300

    added code for task #2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.WriteLine(a);
+}
+else if (b > a & b > c)
+{
+    Console.WriteLine(b);
+}
+else if (c > a & c > b)
+{
+    Console.WriteLine(c);
+}
