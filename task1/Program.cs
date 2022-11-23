17be1570956c6d87d172781c6382c9cce80f5a54
Nataliia <nataliyatrishechkina@mail.ru>
2022-11-24 01:11:46 +0300

    added code for task #1_edit

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,13 +1,4 @@
-﻿// int N = int.Parse(Console.ReadLine());
-
-// // int Nmin = -N;
-
-// // while (N>=Nmin)
-// // {
-// // Console.WriteLine();
-// // N = N-1;
-// // }
-// // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // // a = 5; b = 7 -> max = 7
 // // a = 2 b = 10 -> max = 10
@@ -21,19 +12,27 @@ int min = b;
 
 if (a > b)
 {
-    Console.WriteLine();
-    int max = a;
-    int min = b;
+    Console.Write("max= ");
+    Console.WriteLine(max = a);
+
+    Console.Write("min= ");
+    Console.WriteLine(min = b);
 }
-else
+else if (a < b)
 {
-    Console.WriteLine();
-    int max = b;
-    int min = a;
+    Console.Write("max= ");
+    Console.WriteLine(max = b);
+
+    Console.Write("min= ");
+    Console.WriteLine(min = a);
+}
+else if (a == b)
+{
+    Console.Write("числа равны");
 }
 
-Console.Write("max= ");
-Console.WriteLine(max);
+// Console.Write("max= ");
+// Console.WriteLine(max);
 
-Console.Write("min= ");
-Console.WriteLine(min);
+// Console.Write("min= ");
+// Console.WriteLine(min);
